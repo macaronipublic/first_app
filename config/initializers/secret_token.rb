54c43617518036729b32e824e0fcacb15d685217
macaroni.public <macaroni.public@gmail.com>
2014-03-21 23:12:51 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'fb873cbb2bf5cad5ae317acd04481d7f8be64b12ed6c3357da415a81fbeaf666f09cc83ed3b30720d8181969461285b54e16d34deeee394868570863927ccf28'
